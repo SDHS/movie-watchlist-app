af823d72dc97f7627f5407ad0671c8143a69ad16
Syed Danish Hasan Shah <danishh3@gmail.com>
2024-04-03 22:58:49 +0500

    feat: Add screens for unauthed and empty watchlist

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,3 +1,4 @@
+import ErrorCard from '@/ui/error-card';
 import MovieCard from '@/ui/movie-card';
 import MovieGrid from '@/ui/movie-grid';
 
@@ -7,23 +8,29 @@ export default async function Watchlist() {
   const watchlist = await fetchWatchlist();
 
   if (!watchlist) {
-    return <div>An error occurred. Please try again</div>;
+    return <ErrorCard content="Please sign up to create your watchlist" />;
   }
 
   return (
     <>
       <h1 className="text-bold mb-unit-xl text-3xl">Watchlist</h1>
-      <MovieGrid>
-        {watchlist.map(movie => (
-          <MovieCard
-            href={`/${movie.movieId}`}
-            posterPath={movie.posterPath}
-            name={movie.title}
-            releaseDate={new Date(String(movie.releaseYear))}
-            key={movie.id}
-          />
-        ))}
-      </MovieGrid>
+      {watchlist.length ? (
+        <MovieGrid>
+          {watchlist.map(movie => (
+            <MovieCard
+              href={`/${movie.movieId}`}
+              posterPath={movie.posterPath}
+              name={movie.title}
+              releaseDate={new Date(String(movie.releaseYear))}
+              key={movie.id}
+            />
+          ))}
+        </MovieGrid>
+      ) : (
+        <p className="text-xl font-bold">
+          Add movies to your watchlist to save them here!
+        </p>
+      )}
     </>
   );
 }
