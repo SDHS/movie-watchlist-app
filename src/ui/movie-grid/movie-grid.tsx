3103ed09c54482fc2dada58d516bd89864f71145
Syed Danish Hasan Shah <danishh3@gmail.com>
2024-04-03 22:49:35 +0500

    style: Update min grid item size

--- src/ui/movie-grid/movie-grid.tsx
+++ src/ui/movie-grid/movie-grid.tsx
@@ -2,7 +2,7 @@ import { type Props } from './types';
 
 export default function MovieGrid({ children }: Props) {
   return (
-    <div className="m-auto grid grid-cols-[repeat(auto-fit,_minmax(min(100%,_max(320px,_100%_/_5)),_1fr))] gap-unit-lg">
+    <div className="m-auto grid justify-center grid-cols-[repeat(auto-fit,_minmax(min(100%,_max(320px,_100%_/_5)),_320px))] gap-unit-lg">
       {children}
     </div>
   );
